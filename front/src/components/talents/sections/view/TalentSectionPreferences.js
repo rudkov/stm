b4f8b0e8e095f0b00bddc64257724fadf40c5f5e
Victor Rudkov <victor.rudkov@gmail.com>
2025-05-24 13:42:05 +0100

    Update talent preferences to use array and map instead of a lot of divs.
    Co-authored-by: Roman Selivanov <seroy@bk.ru>

--- front/src/components/talents/sections/view/TalentSectionPreferences.js
+++ front/src/components/talents/sections/view/TalentSectionPreferences.js
@@ -11,120 +11,35 @@ function TalentSectionPreferences(props) {
         <NestedSection className={props.className}>
             <NestedSection.Header>Preferences</NestedSection.Header>
             <NestedSection.Body>
-                <div className='talent-section-preferences__grid'>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_faithbased_ads !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_faithbased_ads]}</div>
-                                    <div className='ellipsis'>Faithbased ads</div>
-                                </>
-                                : <div className='ellipsis text-light'>Faithbased ads</div>
-                        }
-
+              <div className='talent-section-preferences__grid'>
+                {[
+                  ['is_faithbased_ads', 'Faithbased ads'],
+                  ['is_fur', 'Fur'],
+                  ['is_gambling_ads', 'Gambling ads'],
+                  ['is_lingerie', 'Lingerie'],
+                  ['is_liquor_ads', 'Liquor ads'],
+                  ['is_nude', 'Nude'],
+                  ['is_political_ads', 'Political ads'],
+                  ['is_smoking_ads', 'Smoking ads'],
+                  ['is_sports', 'Sports'],
+                  ['is_swimwear', 'Swimwear'],
+                  ['is_topless', 'Topless'],
+                ].map(([key, label]) => {
+                  const value = talent[key];
+                  return (
+                    <div key={key} className='nested-section__item_with_icon ellipsis'>
+                      {value !== null ? (
+                        <>
+                          <div className='nested-section__icon'>{YesNoIcons[value]}</div>
+                          <div className='ellipsis'>{label}</div>
+                        </>
+                      ) : (
+                        <div className='ellipsis text-light'>{label}</div>
+                      )}
                     </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_fur !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_fur]}</div>
-                                    <div className='ellipsis'>Fur</div>
-                                </>
-                                : <div className='ellipsis text-light'>Fur</div>
-                        }
-
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_gambling_ads !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_gambling_ads]}</div>
-                                    <div className='ellipsis'>Gambling ads</div>
-                                </>
-                                : <div className='ellipsis text-light'>Gambling ads</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_lingerie !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_lingerie]}</div>
-                                    <div className='ellipsis'>Lingerie</div>
-                                </>
-                                : <div className='ellipsis text-light'>Lingerie</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_liquor_ads !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_liquor_ads]}</div>
-                                    <div className='ellipsis'>Liquor ads</div>
-                                </>
-                                : <div className='ellipsis text-light'>Liquor ads</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_nude !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_nude]}</div>
-                                    <div className='ellipsis'>Nude</div>
-                                </>
-                                : <div className='ellipsis text-light'>Nude</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_political_ads !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_political_ads]}</div>
-                                    <div className='ellipsis'>Political ads</div>
-                                </>
-                                : <div className='ellipsis text-light'>Political ads</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_smoking_ads !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_smoking_ads]}</div>
-                                    <div className='ellipsis'>Smoking ads</div>
-                                </>
-                                : <div className='ellipsis text-light'>Smoking ads</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_sports !== null
-                                ? <>
-                                    <div className='nested-section__icon'>{YesNoIcons[talent.is_sports]}</div>
-                                    <div className='ellipsis'>Sports</div>
-                                </>
-                                : <div className='ellipsis text-light'>Sports</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_swimwear !== null
-                            ? <>
-                            <div className='nested-section__icon'>{YesNoIcons[talent.is_swimwear]}</div>
-                            <div className='ellipsis'>Swimwear</div>
-                            </>
-                            : <div className='ellipsis text-light'>Swimwear</div>
-                        }
-                    </div>
-                    <div className='nested-section__item_with_icon ellipsis'>
-                        {
-                            talent.is_topless !== null
-                            ? <>
-                            <div className='nested-section__icon'>{YesNoIcons[talent.is_topless]}</div>
-                            <div className='ellipsis'>Topless</div>
-                            </>
-                            : <div className='ellipsis text-light'>Topless</div>
-                        }
-                    </div>
-                </div>
+                  );
+                })}
+              </div>
             </NestedSection.Body>
         </NestedSection>
     );
