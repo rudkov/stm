c7d2f0cd94471f8a97eb07753c2e49ff5b62a186
Victor Rudkov <victor.rudkov@gmail.com>
2025-06-23 10:29:12 +0100

    Refactored ContactRequest. Created CompanyContactRequest.

--- back/app/Http/Requests/ContactRequest.php
+++ back/app/Http/Requests/ContactRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\ValidateEach;
 
 class ContactRequest extends FormRequest
 {
@@ -14,24 +15,14 @@ class ContactRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
+            'first_name' => 'required_without:last_name|string|max:255',
+            'last_name' => 'required_without:first_name|string|max:255',
             'comment' => 'nullable|string',
-            'companies' => 'array|nullable',
-            'companies.*.id' => 'required|exists:companies,id',
-            'companies.*.job_title' => 'nullable|string|max:255',
-            'phones' => 'array|nullable',
-            'phones.*.id' => 'sometimes|exists:phones,id',
-            'phones.*.communication_type_id' => 'required|exists:communication_types,id',
-            'phones.*.info' => 'required|string|max:255',
-            'emails' => 'array|nullable',
-            'emails.*.id' => 'sometimes|exists:emails,id',
-            'emails.*.communication_type_id' => 'required|exists:communication_types,id',
-            'emails.*.info' => 'required|email|max:255',
-            'messengers' => 'array|nullable',
-            'messengers.*.id' => 'sometimes|exists:messengers,id',
-            'messengers.*.messenger_type_id' => 'required|exists:messenger_types,id',
-            'messengers.*.info' => 'required|string|max:255',
+
+            'companies' => ['nullable', 'array', new ValidateEach(new CompanyContactRequest())],
+            'emails' => ['nullable', 'array', new ValidateEach(new EmailRequest())],
+            'messengers' => ['nullable', 'array', new ValidateEach(new MessengerRequest())],
+            'phones' => ['nullable', 'array', new ValidateEach(new PhoneRequest())],
         ];
     }
 }
