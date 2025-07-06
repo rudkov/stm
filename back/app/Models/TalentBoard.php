4f95f5bd683c64497814d1b8abad47ae1f83a1e9
Victor Rudkov <victor.rudkov@gmail.com>
2025-07-06 19:38:52 +0100

    Removed timestamps, soft deletes, and user tracking from TalentBoards. Added indexes to TalentBoard migrations and relations.

--- back/app/Models/TalentBoard.php
+++ back/app/Models/TalentBoard.php
@@ -4,25 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 use App\Models\Talent;
 
 use App\Traits\BelongsToTeam;
-use App\Traits\HasUserTracking;
 
 class TalentBoard extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     use BelongsToTeam;
-    use HasUserTracking;
+
+    public $timestamps = false;
 
     protected $fillable = [
         'name',
-        'created_by',
-        'updated_by',
     ];
 
     public function talents()
--- back/database/factories/TalentBoardFactory.php
+++ back/database/factories/TalentBoardFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\TalentBoard;
 use App\Models\Team;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TalentBoardFactory extends Factory
@@ -16,12 +15,6 @@ class TalentBoardFactory extends Factory
         return [
             'name' => $this->faker->words(3, true),
             'team_id' => Team::factory(),
-            'created_by' => function (array $attributes) {
-                return User::factory()->create(['team_id' => $attributes['team_id']])->id;
-            },
-            'updated_by' => function (array $attributes) {
-                return $attributes['created_by'];
-            },
         ];
     }
-} 
+}
