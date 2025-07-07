2f24a88760d0fdbc6de034067dbb459efac5adf7
Victor Rudkov <victor.rudkov@gmail.com>
2025-07-07 20:35:08 +0100

    Added support for Address and SocialMedia to Contacts

--- back/app/Http/Resources/ContactResource.php
+++ back/app/Http/Resources/ContactResource.php
@@ -19,13 +19,19 @@ class ContactResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'comment' => $this->comment,
+
+            // Collections
+            'addresses'     => $this->whenLoaded('addresses',    fn() => AddressResource::collection($this->addresses)),
+            'companies'     => $this->whenLoaded('companies',    fn() => ContactCompanyResource::collection($this->companies)),
+            'emails'        => $this->whenLoaded('emails',       fn() => EmailResource::collection($this->emails)),
+            'messengers'    => $this->whenLoaded('messengers',   fn() => MessengerResource::collection($this->messengers)),
+            'phones'        => $this->whenLoaded('phones',       fn() => PhoneResource::collection($this->phones)),
+            'social_medias' => $this->whenLoaded('socialMedias', fn() => SocialMediaResource::collection($this->socialMedias)),
+            'weblinks'      => $this->whenLoaded('weblinks',     fn() => WeblinkResource::collection($this->weblinks)),
+
+            // Timestamps and user tracking
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'companies' => ContactCompanyResource::collection($this->whenLoaded('companies')),
-            'phones' => PhoneResource::collection($this->whenLoaded('phones')),
-            'emails' => EmailResource::collection($this->whenLoaded('emails')),
-            'messengers' => MessengerResource::collection($this->whenLoaded('messengers')),
-            'weblinks' => WeblinkResource::collection($this->whenLoaded('weblinks')),
             'created_by' => $this->whenLoaded('createdBy', fn() => new UserBasicResource($this->createdBy)),
             'updated_by' => $this->whenLoaded('updatedBy', fn() => new UserBasicResource($this->updatedBy)),
         ];
