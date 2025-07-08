6029a4b130eeb9a11636610d9e7430cda617210d
Victor Rudkov <victor.rudkov@gmail.com>
2025-07-08 07:45:31 +0100

    Removed redundant comments from all resources

--- back/app/Http/Resources/CompanyResource.php
+++ back/app/Http/Resources/CompanyResource.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CompanyResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
--- back/app/Http/Resources/ContactCompanyResource.php
+++ back/app/Http/Resources/ContactCompanyResource.php
@@ -5,21 +5,15 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-
 class ContactCompanyResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'job_title' => $this->whenPivotLoaded('company_contact', function () {
-               return $this->pivot->job_title;
+                return $this->pivot->job_title;
             }),
         ];
     }
