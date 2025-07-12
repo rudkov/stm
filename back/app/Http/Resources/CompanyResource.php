31a289381d53a036c2b735c63a3246784574d744
Victor Rudkov <victor.rudkov@gmail.com>
2025-07-12 20:24:17 +0100

    Improved code formatting even more

--- back/app/Http/Resources/CompanyResource.php
+++ back/app/Http/Resources/CompanyResource.php
@@ -14,13 +14,13 @@ class CompanyResource extends JsonResource
             'name' => $this->name,
 
             // Collections
-            'addresses'     => $this->whenLoaded('addresses', fn() => AddressResource::collection($this->addresses)),
-            'contacts'      => $this->whenLoaded('contacts', fn() => ContactBasicResource::collection($this->contacts)),
-            'emails'        => $this->whenLoaded('emails', fn() => EmailResource::collection($this->emails)),
-            'messengers'    => $this->whenLoaded('messengers', fn() => MessengerResource::collection($this->messengers)),
-            'phones'        => $this->whenLoaded('phones', fn() => PhoneResource::collection($this->phones)),
+            'addresses' => $this->whenLoaded('addresses', fn() => AddressResource::collection($this->addresses)),
+            'contacts' => $this->whenLoaded('contacts', fn() => ContactBasicResource::collection($this->contacts)),
+            'emails' => $this->whenLoaded('emails', fn() => EmailResource::collection($this->emails)),
+            'messengers' => $this->whenLoaded('messengers', fn() => MessengerResource::collection($this->messengers)),
+            'phones' => $this->whenLoaded('phones', fn() => PhoneResource::collection($this->phones)),
             'social_medias' => $this->whenLoaded('socialMedias', fn() => SocialMediaResource::collection($this->socialMedias)),
-            'weblinks'      => $this->whenLoaded('weblinks', fn() => WeblinkResource::collection($this->weblinks)),
+            'weblinks' => $this->whenLoaded('weblinks', fn() => WeblinkResource::collection($this->weblinks)),
 
             // Timestamps and user tracking
             'created_at' => $this->created_at,
